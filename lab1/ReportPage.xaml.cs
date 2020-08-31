e1e29923126a8129932fd51259231e128ebfeaec
Daniel <danik5311@gmail.com>
2020-08-31 16:04:32 +0300

    [REFACTORING][ADD] Rearrange folders. Add lab1.

---
+++ lab1/ReportPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace lab1
+{
+    public partial class ReportPage : Page
+    {
+        public ReportPage()
+        {
+            InitializeComponent();
+        }
+        
+        public ReportPage(object data):this()
+        {
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            var expenseReportPage = new ReportPage();
+            this.NavigationService?.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ lab1/labHome.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace lab1
+{
+    public partial class LabHome : Page
+    {
+        public LabHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            var expenseReportPage = new ReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+
+        }
+    }
+}
