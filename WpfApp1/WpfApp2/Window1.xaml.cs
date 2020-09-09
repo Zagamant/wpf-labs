60f6d80573ecd50dba6d92358e32ce8bc5e0bfd3
Daniil <danik5311@gmail.com>
2020-09-09 13:21:04 +0300

    [UPDATE] Update task2 namespaces

--- WpfApp1/WpfApp2/Window1.xaml.cs
+++ WpfApp1/WpfApp2/Window1.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace WpfApp2
+namespace Task2
 {
     public partial class Window1 : Window
     {
